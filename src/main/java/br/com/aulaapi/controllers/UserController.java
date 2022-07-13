639089fd8d6c3feb08bb93d38738071f79cab0c7
Felipe Lima Jardim <felipe@MacBook-Pro-de-Felipe.local>
2022-07-13 16:45:05 -0400

    criado endpoints e metodos para retornar um usuario específico e deletar usuario

--- src/main/java/br/com/aulaapi/controllers/UserController.java
+++ src/main/java/br/com/aulaapi/controllers/UserController.java
@@ -2,9 +2,13 @@ package br.com.aulaapi.controllers;
 
 import br.com.aulaapi.entities.User;
 import br.com.aulaapi.services.UserService;
+import org.hibernate.ObjectNotFoundException;
+import org.hibernate.annotations.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.Serializable;
 import java.util.List;
 
 @RestController
@@ -18,10 +22,19 @@ public class UserController {
     @ResponseBody
     public List<User> searchUser(){
         return userService.searchUser();
-    }
+    }       //ok
+
+    //ver como faz para retornar uma mensagem de erro
+    @GetMapping("/{id}")
+    public User returnUser(@PathVariable(value = "id") Long id){
+        return userService.returnUser(id);
+    }       //ok
 
     @PostMapping()
     public User createUser(@RequestBody User user){
         return userService.createUser(user);
-    }
+    }       //ok
+
+    @DeleteMapping("/{id}")
+    public Boolean deleteUser(@RequestBody User user){ return userService.deleteUser(user.getId()); }
 }
--- src/main/java/br/com/aulaapi/services/UserService.java
+++ src/main/java/br/com/aulaapi/services/UserService.java
@@ -1,8 +1,11 @@
 package br.com.aulaapi.services;
 
+import br.com.aulaapi.entities.Phone;
 import br.com.aulaapi.entities.User;
 import br.com.aulaapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +20,22 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public User returnUser(Long id){
+        return userRepository.findById(id).orElse(null);
+    }
+
     public User createUser(User user){
+        User u = userRepository.save(user);
+        return u;
+    }
+
+    public User updateUser(User user){
         return userRepository.save(user);
+    }
+
+    public Boolean deleteUser (long id){
+        userRepository.deleteById(id);
+        User u = userRepository.findById(id).orElse(null);
+        return u.equals(null);
     }
 }
